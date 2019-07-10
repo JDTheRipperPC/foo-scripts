7a767bd5724ed87370ae259cda517042a96c17a7
JDTheRipperPC <jdtheripperpc@gmail.com>
2019-07-10 13:41:50 +0200

    Implemented python/whois_sample.py, not a lazy pajjet anymore

--- python/whois_sample.py
+++ python/whois_sample.py
@@ -1,3 +1,14 @@
+import pprint
 import pythonwhois
 
-# TODO: Implement me lazy pajjet
+
+root_server = pythonwhois.net.get_root_server('google.com')
+print(root_server)
+print()
+
+whois = pythonwhois.get_whois('google.com')
+print(whois.items())
+print()
+
+whois_raw = pythonwhois.net.get_whois_raw('google.com')
+pprint.pprint(whois_raw)
